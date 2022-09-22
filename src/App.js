e78516fdf079cff23fd7ac9f9075ced03a8ab6bc
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-09-22 23:10:22 +0300

    🔨 Fix some linters error

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Portfolio />
-      <Resume/>
+      <Resume />
     </div>
   );
 }
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,50 +1,48 @@
-import React from "react"
-import "./Resume.css"
-import ResumeApi from "./ResumeApi"
-import Card from "./Card"
+import React from 'react';
+import './Resume.css';
+import ResumeApi from './ResumeApi';
+import Card from './Card';
 
-const Resume = () => {
-  return (
-    <>
-      <section className='Resume' id='resume'>
-        <div className='container top'>
-          <div className='heading text-center'>
-            <h1>My Resume</h1>
-          </div>
+const Resume = () => (
+  <>
+    <section className="Resume" id="resume">
+      <div className="container top">
+        <div className="heading text-center">
+          <h1>My Resume</h1>
+        </div>
 
-          <div className='content-section mtop d_flex'>
-            <div className='left'>
-              <div className='heading'>
-                <h1>Education Quality</h1>
-              </div>
-
-              <div className='content'>
-                {ResumeApi.map((val, id) => {
-                  if (val.category === "education") {
-                    return <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
-                  }
-                })}
-
-              </div>
+        <div className="content-section mtop d_flex">
+          <div className="left">
+            <div className="heading">
+              <h1>Education Quality</h1>
             </div>
-            <div className='left'>
-              <div className='heading'>
-                <h1>Job Experience</h1>
-              </div>
 
-              <div className='content'>
-                {ResumeApi.map((val, index) => {
-                  if (val.category === "experience") {
-                    return <Card key={index} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
-                  }
-                })}
-              </div>
+            <div className="content">
+              {ResumeApi.map((val, id) => {
+                if (val.category === 'education') {
+                  return <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />;
+                }
+              })}
+
+            </div>
+          </div>
+          <div className="left">
+            <div className="heading">
+              <h1>Job Experience</h1>
+            </div>
+
+            <div className="content">
+              {ResumeApi.map((val, index) => {
+                if (val.category === 'experience') {
+                  return <Card key={index} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />;
+                }
+              })}
             </div>
           </div>
         </div>
-      </section>
-    </>
-  )
-}
+      </div>
+    </section>
+  </>
+);
 
-export default Resume
+export default Resume;
