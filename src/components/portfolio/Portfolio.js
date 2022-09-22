563ba8a195b57cadecee82c4c5e78c91d7a25e21
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-09-22 21:48:26 +0300

    File name changed and fix some linters error

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,27 +1,23 @@
-import React from "react"
-import "./Portfolio.css"
-import Card from "./Card"
-import Portfolio_data from "./Portfolio_data.js"
+import React from 'react';
+import './Portfolio.css';
+import Card from './Card';
+import Portfolio_data from './portfolioData.js';
 
-const Portfolio = () => {
-  return (
-    <>
-      <section className='Portfolio top' id='portfolio'>
-        <div className='container'>
-          <div className='heading text-center '>
-            <h4>VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK</h4>
-            <h1>My Portfolio</h1>
-          </div>
-
-          <div className='content grid'>
-            {Portfolio_data.map((value, index) => {
-              return <Card key={index} image={value.image} category={value.category} tech={value.tech} title={value.title} />
-            })}
-          </div>
+const Portfolio = () => (
+  <>
+    <section className="Portfolio top" id="portfolio">
+      <div className="container">
+        <div className="heading text-center ">
+          <h4>VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK</h4>
+          <h1>My Portfolio</h1>
         </div>
-      </section>
-    </>
-  )
-}
 
-export default Portfolio
+        <div className="content grid">
+          {Portfolio_data.map((value, index) => <Card key={index} image={value.image} category={value.category} tech={value.tech} title={value.title} />)}
+        </div>
+      </div>
+    </section>
+  </>
+);
+
+export default Portfolio;
---
+++ src/components/portfolio/portfolioData.js
@@ -0,0 +1,59 @@
+const Portfolio_data = [
+  {
+    id: 1,
+    category: 'Web site',
+    tech: 'React',
+    title: 'Cloud App',
+    image: 'https://raw.githubusercontent.com/sentayhu19/Portfolio/Portfolio/src/assets/images/project-screenshoot/cloud.png',
+  },
+  {
+    id: 2,
+    category: 'Web site',
+    tech: 'JavaScript | HTML | CSS',
+    title: ' ShowBox Movies.',
+    image: 'https://raw.githubusercontent.com/sentayhu19/Portfolio/Portfolio/src/assets/images/project-screenshoot/movie.png',
+  },
+  {
+    id: 3,
+    category: 'Node package',
+    tech: 'TypeScript',
+    title: 'Random String Generator Package',
+    image: 'https://raw.githubusercontent.com/sentayhu19/Portfolio/Portfolio/src/assets/images/project-screenshoot/random-string-gen.png',
+  },
+  {
+    id: 4,
+    category: 'web site',
+    tech: 'React',
+    title: 'Covid-19 info Tracker',
+    image: 'https://raw.githubusercontent.com/sentayhu19/Portfolio/Portfolio/src/assets/images/project-screenshoot/covid19.png',
+  },
+  {
+    id: 5,
+    category: 'WEB app',
+    tech: 'PHP | HTML | CSS | JavaScript',
+    title: 'E-voting system for ethiopia',
+    image: 'https://raw.githubusercontent.com/sentayhu19/Portfolio/Portfolio/src/assets/images/project-screenshoot/e-voting.png',
+  },
+  {
+    id: 6,
+    category: 'Web app',
+    tech: 'React Firebase',
+    title: 'Arada Chat',
+    image: 'https://raw.githubusercontent.com/sentayhu19/Portfolio/Portfolio/src/assets/images/project-screenshoot/arada-chat.png',
+  },
+  {
+    id: 7,
+    category: 'Web app',
+    tech: 'React',
+    title: 'Bookstore',
+    image: 'https://raw.githubusercontent.com/sentayhu19/Portfolio/Portfolio/src/assets/images/project-screenshoot/bookstore.png',
+  },
+  {
+    id: 8,
+    category: 'Web app',
+    tech: 'JavaScript',
+    title: 'Leaderboard',
+    image: 'https://raw.githubusercontent.com/sentayhu19/Portfolio/Portfolio/src/assets/images/project-screenshoot/leaderboard.png',
+  },
+];
+export default Portfolio_data;
