f54d6fa620584a79441921ee5d07bfc6696217eb
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-09-23 12:30:43 +0300

    ✨ Blog component created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Head/Header';
 import Hero from './components/Hero/Hero';
 import Portfolio from './components/portfolio/Portfolio';
 import Resume from './components/Resume/Resume';
+import Blog from './components/Blog/Blog';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Portfolio />
       <Resume />
+      <Blog />
     </div>
   );
 }
