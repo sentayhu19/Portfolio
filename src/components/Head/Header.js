2b533c693fccf783b5d61a2bc4d47141e2bef51b
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-10-02 21:58:04 +0300

    react scroll added

--- src/components/Head/Header.js
+++ src/components/Head/Header.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import './Header.css';
-import logo from '../../assets/images/logo.png';
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import "./Header.css";
+import logo from "../../assets/images/logo.png";
 
 const Header = () => {
   // scrolling header at the top of the page
-  window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header');
-    header.classList.toggle('active', window.scrollY > 100);
+  window.addEventListener("scroll", () => {
+    const header = document.querySelector(".header");
+    header.classList.toggle("active", window.scrollY > 100);
   });
   const [Mobile, setMobile] = useState(false);
   return (
@@ -17,29 +18,98 @@ const Header = () => {
             <img src={logo} alt="Logo" data-aos="flip-up" className="logo" />
           </div>
           <div className="navlink">
-            {/* <ul className='link f_flex uppercase'> */}
-            <ul className={Mobile ? 'nav-links-mobile' : 'link f_flex uppercase'} onClick={() => setMobile(false)}>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#portfolio">Portfolio</a></li>
-              <li><a href="#resume">Resume</a></li>
-              <li><a href="#blog">Blog</a></li>
+            <ul
+              className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"}
+              onClick={() => setMobile(false)}
+            >
+              <li>
+                {" "}
+                <Link
+                  activeClass="active"
+                  to="home"
+                  spy
+                  smooth
+                  offset={50}
+                  duration={500}
+                >
+                  {" "}
+                  Home
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link
+                  activeClass="active"
+                  to="portfolio"
+                  spy
+                  smooth
+                  offset={-150}
+                  duration={500}
+                >
+                  {" "}
+                  Portfolio
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link
+                  activeClass="active"
+                  to="resume"
+                  spy
+                  smooth
+                  offset={-140}
+                  duration={500}
+                >
+                  {" "}
+                  Resume
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link
+                  activeClass="active"
+                  to="blog"
+                  spy
+                  smooth
+                  offset={-140}
+                  duration={500}
+                >
+                  {" "}
+                  Blog
+                </Link>
+              </li>
               <li>
                 <button type="button" className="home-btn">
-                  <a href="https://github.com/sentayhu19" target="_blank" rel="noreferrer">
+                  <a
+                    href="https://github.com/sentayhu19"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <i className="fab fa-github" />
                   </a>
                 </button>
               </li>
-              <li><button type="button" className="home-btn"><a href="#contact">Hire Me</a></button></li>
+              <li>
+                <button type="button" className="home-btn">
+                  <a href="#contact">Hire Me</a>
+                </button>
+              </li>
             </ul>
-            <button type="button" className="toggle" onClick={() => setMobile(!Mobile)}>
-              {Mobile ? <i className="fas fa-times close home-btn" /> : <i className="fas fa-bars open" />}
+            <button
+              type="button"
+              className="toggle"
+              onClick={() => setMobile(!Mobile)}
+            >
+              {Mobile ? (
+                <i className="fas fa-times close home-btn" />
+              ) : (
+                <i className="fas fa-bars open" />
+              )}
             </button>
           </div>
         </div>
       </header>
     </>
-
   );
 };
 
